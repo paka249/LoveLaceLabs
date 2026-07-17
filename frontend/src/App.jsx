cb3af46d0ffe560254016919b2783b41e6d8ae5f
Angel Verde <usn3@txstate.edu>
2026-07-16 22:50:59 -0500

    Kept ADA permaeantly in the collapased bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,6 @@ export default function App() {
       <Sidebar
         open={sidebarOpen}
         onToggle={() => setSidebarOpen((v) => !v)}
-        chatbotDismissed={chatbotDismissed}
         onRestoreChatbot={() => setChatbotDismissed(false)}
       />
 
